17c538a495589c0071da90b9574c33b41150acf8
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-09-20 20:21:53 -0300

    att

---
+++ src/controllers/contatoController.js
@@ -0,0 +1,37 @@
+const Contato = require('../models/ContatoModel');
+
+exports.index = (req, res) => {
+    res.render('contato' , {
+        contato: {}
+    });
+};
+
+exports.register = async (req, res) => {
+    try {
+        const contato = new Contato(req.body);
+        await contato.register();
+
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('/contato/index'));
+            return;
+        }
+
+        req.flash('success', 'Contato registrado com sucesso');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.editIndex = async (req, res) => {
+    if(!req.params.id) return res.render('404');
+    const contatoInstance = new Contato(req.body);
+    const contato = await contatoInstance.buscaPorId(req.params.id);
+
+    if(!contato) return res.render('404');
+
+    res.render('contato', { contato });
+};
