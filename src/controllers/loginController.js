4195b0e5dc2ca3a2ffb0f85f7a48454a7eb55ac7
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2025-09-20 18:20:51 -0300

    Criando as funcionalidade de logar no sistema

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-    res.render('login');
-    return;
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async (req, res) => {
@@ -16,18 +16,45 @@ exports.register = async (req, res) => {
                 return res.redirect('/login/index');
             });
             return;
-        };
+        }
 
         req.flash('success', 'Usuário criado com sucesso!');
         req.session.save(() => {
             return res.redirect('/login/index');
-        });
+        })
 
         
     } catch(e) {
         console.log(e);
         return req.render('404');
     }
-    
-    
 };
+
+exports.login = async (req, res) => {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(() => {
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema');
+        req.session.user = login.user;
+        req.session.save(() => {
+            return res.redirect('/login/index');
+        });
+    } catch(e) {
+        console.log(e);
+        return res.rende('404');
+    }
+};
+
+exports.logout = (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+}
